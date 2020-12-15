7eab2c790a1761557a76a4a934694eb8ee54d29f
AlissonPC <batista.als@gmail.com>
2020-12-15 19:42:04 -0300

    Update detecta-faces.py

--- detecta-faces.py
+++ detecta-faces.py
@@ -1,17 +1,23 @@
-import cv2
+import cv2 as cv
+import numpy as np
 
 print('Carregando código.....')
 
 #Realiza o loading do modelo treinado
-loadingCls = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 #Seleciona a imagem
-imagem = cv2.imread('img/imagem1.jpg')
+imagem = cv.imread('img/imagem4.jpg')
 
-imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+imagemCinza = cv.cvtColor(imagem, cv.COLOR_BGR2GRAY)
 
-detecta = loadingCls.detectMultiScale(imagemCinza, scaleFactor=1.1, minNeighbors=8, minSize=(25, 25))
+detecta_face = face_cascade.detectMultiScale(imagemCinza)
 
+#Percorre faces/rostos
+for (x, y, l, a) in detecta_face:
+    cv.rectangle(imagem,(x,y),(x+l,y+a),(255,0,0),2)
 
-cv2.imshow("Faces", detecta)
-cv2.waitKey()
+print('Face detectada!!.....')
+cv.imshow("FACE", imagem)
+cv.waitKey()
+cv.destroyAllWindows()
